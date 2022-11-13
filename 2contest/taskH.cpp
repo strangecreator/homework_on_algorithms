a91fd54f7031d75f32673bab74946de176e7c89f
Solomon <52523572+strangecreator@users.noreply.github.com>
2022-11-13 23:39:57 +0300

    Update taskH.cpp

--- 2contest/taskH.cpp
+++ 2contest/taskH.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <utility>
 
-int Devide(int n, int k) {
+static int min_amount = 5;
+
+int CeilDivide(int n, int k) {
   if (n % k == 0) {
     return n / k;
   }
@@ -11,17 +13,17 @@ int Devide(int n, int k) {
 
 // sorts an array that contains at most five elements,
 // and returns a median
-int Sort5(int* array, int left, int right) {
+int SortAndGetMedian(int* array, int left, int right) {
   // bubble sort
-  for (int i = left; i < right; i++) {
-    for (int j = i + 1; j <= right; j++) {
+  for (int i = left; i < right; ++i) {
+    for (int j = i + 1; j <= right; ++j) {
       if (array[i] > array[j]) {
         std::swap(array[i], array[j]);
       }
     }
   }
   // returns a median
-  return left + Devide(right - left + 1, 2) - 1;
+  return left + CeilDivide(right - left + 1, 2) - 1;
 }
 
 // sorts an array relative to a certain element,
@@ -30,7 +32,7 @@ int Partition(int* array, int left, int right, int pivot_index) {
   // finds elements that less than or equal to pivot
   std::swap(array[left], array[pivot_index]);
   int j = left + 1;
-  for (int i = left + 1; i <= right; i++) {
+  for (int i = left + 1; i <= right; ++i) {
     if (array[i] <= array[left]) {
       std::swap(array[j++], array[i]);
     }
@@ -38,13 +40,13 @@ int Partition(int* array, int left, int right, int pivot_index) {
   std::swap(array[left], array[j - 1]);
   // finds elements that less than pivot
   int u = left;
-  for (int i = left; i < j; i++) {
+  for (int i = left; i < j; ++i) {
     if (array[i] < array[j - 1]) {
       std::swap(array[u], array[i]);
-      u++;
+      ++u;
     }
   }
-  int middle = left + Devide(right - left + 1, 2) - 1;
+  int middle = left + CeilDivide(right - left + 1, 2) - 1;
   if (u <= middle && middle <= j - 1) {
     return middle;
   }
@@ -54,13 +56,13 @@ int Partition(int* array, int left, int right, int pivot_index) {
   return j - 1;
 }
 
-int MedianOfMedians(int* array, int left, int right);
+int ApproximateMedian(int* array, int left, int right);
 
 // returns an index to a kth smallest element (array will be modified!)
-int Select(int* array, int left, int right, int k) {
-  while (right - left >= 5) {
+int KthSmallest(int* array, int left, int right, int k) {
+  while (right - left >= min_amount) {
     // finds an approximate median index in [left, right] segment
-    int median_index = MedianOfMedians(array, left, right);
+    int median_index = ApproximateMedian(array, left, right);
     // sorts [left, right] segment relatively to an array[medianIndex]
     // and returns an index to this element
     int pivot_index = Partition(array, left, right, median_index);
@@ -74,32 +76,33 @@ int Select(int* array, int left, int right, int k) {
       right = pivot_index - 1;
     }
   }
-  Sort5(array, left, right);
+  SortAndGetMedian(array, left, right);
   return left + k - 1;
 }
 
 // returns an approximate median of an array (array will be modified!)
-int MedianOfMedians(int* array, int left, int right) {
-  for (int i = left; i <= right; i += 5) {
-    int sub_right = (i + 4 < right) ? i + 4 : right;
-    int median_index = Sort5(array, i, sub_right);
-    std::swap(array[median_index], array[left + (i - left) / 5]);
+int ApproximateMedian(int* array, int left, int right) {
+  for (int i = left; i <= right; i += min_amount) {
+    int sub_right = (i + min_amount - 1 < right) ? i + min_amount - 1 : right;
+    int median_index = SortAndGetMedian(array, i, sub_right);
+    std::swap(array[median_index], array[left + (i - left) / min_amount]);
   }
-  int new_right = left + Devide(right - left + 1, 5) - 1;
-  int middle_position = Devide(new_right - left + 1, 2);
-  return Select(array, left, new_right, middle_position);
+  int new_right = left + CeilDivide(right - left + 1, min_amount) - 1;
+  int middle_position = CeilDivide(new_right - left + 1, 2);
+  return KthSmallest(array, left, new_right, middle_position);
 }
 
 void QuickSort(int* array, int left, int right) {
   if (right - left >= 5) {
-    int median_index = MedianOfMedians(array, left, right);
+    int middle_position = (right - left + 1) >> 1;
+    int median_index = KthSmallest(array, left, right, middle_position);
     int pivot_index = Partition(array, left, right, median_index);
     QuickSort(array, left, pivot_index);
     if (pivot_index < right) {
       QuickSort(array, pivot_index + 1, right);
     }
   } else {
-    Sort5(array, left, right);
+    SortAndGetMedian(array, left, right);
   }
 }
 
@@ -109,11 +112,11 @@ int main() {
   int n;
   std::cin >> n;
   int* array = new int[n];
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; ++i) {
     std::cin >> array[i];
   }
   QuickSort(array, 0, n - 1);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; ++i) {
     std::cout << array[i] << " ";
   }
   delete[] array;
