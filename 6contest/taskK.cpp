8b8213a606d1bd7c56e71f56a5d8722662329a4e
Solomon <52523572+strangecreator@users.noreply.github.com>
2023-03-27 12:10:12 +0300

    Update taskK.cpp

--- 6contest/taskK.cpp
+++ 6contest/taskK.cpp
@@ -8,6 +8,69 @@ struct DPCell {
   int amount_selected = 0;
 };
 
+class Backpack {
+  int n_, m_;
+  int* w_;  // weight
+  int* c_;  // cost
+  DPCell** dp_;
+
+ public:
+  Backpack(int n, int m)
+      : n_(n), m_(m), w_(new int[n]), c_(new int[n]), dp_(new DPCell*[n + 1]) {}
+
+  ~Backpack() {
+    delete[] w_;
+    delete[] c_;
+    for (int i = 0; i <= n_; ++i) {
+      delete[] dp_[i];
+    }
+    delete[] dp_;
+  }
+
+  void Read(std::istream& stream) {
+    for (int i = 0; i < n_; ++i) {
+      stream >> w_[i];
+    }
+    for (int i = 0; i < n_; ++i) {
+      stream >> c_[i];
+    }
+  }
+
+  int Solve() {
+    for (int i = 0; i <= n_; ++i) {
+      dp_[i] = new DPCell[m_ + 1];
+      for (int j = 0; j <= m_; ++j) {
+        if (i == 0 || j == 0) {
+          dp_[i][j] = {0, false, 0};
+          continue;
+        }
+        dp_[i][j] = std::max(
+            {DPCell{dp_[i - 1][j].cost, false, dp_[i - 1][j].amount_selected},
+             (j >= w_[i - 1])
+                 ? DPCell{dp_[i - 1][j - w_[i - 1]].cost + c_[i - 1], true,
+                          dp_[i - 1][j - w_[i - 1]].amount_selected + 1}
+                 : DPCell()},
+            [](const DPCell& c1, const DPCell& c2) -> bool {
+              return c1.cost < c2.cost;
+            });
+      }
+    }
+    return dp_[n_][m_].amount_selected;
+  }
+
+  void FindPath(int* indices) const {
+    int i = n_, j = m_;
+    for (int u = dp_[n_][m_].amount_selected - 1; u >= 0; --u) {
+      while (!dp_[i][j].in_use) {
+        --i;
+      }
+      indices[u] = i;
+      j -= w_[i - 1];
+      --i;
+    }
+  }
+};
+
 int main() {
   // speeding up
   std::ios::sync_with_stdio(false);
@@ -16,53 +79,15 @@ int main() {
   // initialization
   int n, m;
   std::cin >> n >> m;
-  int* w = new int[n];
-  int* c = new int[n];
-  for (int i = 0; i < n; ++i) {
-    std::cin >> w[i];
-  }
-  for (int i = 0; i < n; ++i) {
-    std::cin >> c[i];
-  }
-  DPCell** dp = new DPCell*[n + 1];
-  for (int i = 0; i <= n; ++i) {
-    dp[i] = new DPCell[m + 1];
-    for (int j = 0; j <= m; ++j) {
-      if (i == 0 || j == 0) {
-        dp[i][j] = {0, false, 0};
-        continue;
-      }
-      dp[i][j] = std::max(
-          {DPCell{dp[i - 1][j].cost, false, dp[i - 1][j].amount_selected},
-           (j >= w[i - 1])
-               ? DPCell{dp[i - 1][j - w[i - 1]].cost + c[i - 1], true,
-                        dp[i - 1][j - w[i - 1]].amount_selected + 1}
-               : DPCell()},
-          [](const DPCell& c1, const DPCell& c2) -> bool {
-            return c1.cost < c2.cost;
-          });
-    }
-  }
+  Backpack backpack(n, m);
+  backpack.Read(std::cin);
+  int result = backpack.Solve();
   // restoring the path
-  int* indices = new int[dp[n][m].amount_selected];
-  int i = n, j = m;
-  for (int u = dp[n][m].amount_selected - 1; u >= 0; --u) {
-    while (!dp[i][j].in_use) {
-      --i;
-    }
-    indices[u] = i;
-    j -= w[i - 1];
-    --i;
-  }
-  for (int i = 0; i < dp[n][m].amount_selected; ++i) {
+  int* indices = new int[result];
+  backpack.FindPath(indices);
+  for (int i = 0; i < result; ++i) {
     std::cout << indices[i] << std::endl;
   }
   // freeing up memory
-  delete[] w;
-  delete[] c;
-  for (int i = 0; i <= n; ++i) {
-    delete[] dp[i];
-  }
-  delete[] dp;
   delete[] indices;
 }
